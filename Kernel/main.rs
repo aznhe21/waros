7cb6e69af93f1a29fc2c67b7ecb91f59d231fe50
aznhe21 <aznhe21.mc@gmail.com>
2015-12-02 14:49:24 +0900

    足りないfeatureを追加

--- Kernel/main.rs
+++ Kernel/main.rs
@@ -16,7 +16,7 @@
 // Unstable language features
 #![feature(associated_consts, const_fn, concat_idents)]
 // Unstable library features
-#![feature(core_intrinsics, num_bits_bytes, ptr_as_ref, drop_in_place, heap_api)]
+#![feature(core_intrinsics, zero_one, num_bits_bytes, ptr_as_ref, drop_in_place, heap_api)]
 
 #![cfg_attr(any(target_arch="x86_64", target_arch="x86"), feature(step_by, iter_arith))]
 
