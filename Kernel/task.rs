44848fe3d1f7b740efdf59560501fd7c10a3af54
aznhe21 <aznhe21.mc@gmail.com>
2016-01-07 00:09:32 +0900

    タスク切り替えを改善

--- Kernel/task.rs
+++ Kernel/task.rs
@@ -54,14 +54,14 @@ pub enum State {
     Free
 }
 
-#[derive(PartialEq, Eq, Debug, Clone, Copy)]
+#[derive(PartialEq, Eq, PartialOrd, Ord,Debug, Clone, Copy)]
 #[repr(u8)]
 pub enum Priority {
-    Critical = 0,
-    High,
-    Middle,
-    Low,
-    Idle
+    Critical = 4,
+    High     = 3,
+    Middle   = 2,
+    Low      = 1,
+    Idle     = 0
 }
 
 const PRIORITY_LEN: usize = 5;
@@ -244,18 +244,34 @@ impl TaskManager {
         }
     }
 
-    // 実行可能状態タスクのある最も高い優先度を返す
+    // 実行可能状態タスクのある最も高い優先度を返す。
     fn highest_priority(&self) -> Priority {
         let (priority, _) = self.runnable_tasks.iter()
             .enumerate()
+            .rev()
             .find(|&(_, tasks)| !tasks.is_empty())
             .unwrap();
         Priority::from_u8(priority as u8)
     }
 
+    // 指定したタスク以外で、実行可能状態タスクのある最も高い優先度を返す。
+    fn highest_priority_without(&self, task: &Task) -> Priority {
+        let (priority, _) = self.runnable_tasks.iter()
+            .enumerate()
+            .rev()
+            // !tasks.is_empty() && tasks != [task]
+            .find(|&(_, tasks)| match tasks.len() {
+                0 => false,
+                1 => tasks.front().map_or(ptr::null_mut(), |p| *p) != *task.ptr,
+                _ => true
+            })
+            .unwrap();
+        Priority::from_u8(priority as u8)
+    }
+
     #[inline]
-    fn current_tasks(&mut self) -> &mut DList<TaskData> {
-        &mut self.runnable_tasks[self.current_priority as usize]
+    fn current_tasks(&self) -> &DList<TaskData> {
+        &self.runnable_tasks[self.current_priority as usize]
     }
 
     #[inline]
@@ -294,16 +310,32 @@ impl TaskManager {
             }
         };
 
-        self.runnable_tasks.iter().any(&list_has_task) || list_has_task(&self.suspended_tasks)
+        self.runnable_tasks.iter().rev().any(&list_has_task) || list_has_task(&self.suspended_tasks)
     }
 
     #[inline]
-    pub fn reset_timer(&mut self) {
+    fn reset_timer(&mut self) {
         self.timer.reset(arch::task::TASK_SWITCH_INTERVAL);
     }
 
+    #[inline]
+    fn is_switch_needed(&self) -> bool {
+        self.highest_priority() > self.current_priority
+    }
+
+    #[inline]
+    fn can_switch(&self) -> bool {
+        self.current_tasks().len() != 1 || self.is_switch_needed()
+    }
+
     fn switch_by_timer(_: timer::TimerId) {
-        manager().yield_now();
+        let mut man = manager();
+
+        if man.can_switch() {
+            man.switch_to_next();
+        } else {
+            man.reset_timer();
+        }
     }
 
     fn resume_by_timer(timer: timer::TimerId) {
@@ -321,10 +353,14 @@ impl TaskManager {
         if task.is_valid() {
             let data = task.data();
             match data.state {
-                State::Runnable => {
+                State::Runnable => if data.priority != priority {
                     self.remove_task(task.ptr);
                     data.priority = priority;
                     self.push_task(task.ptr);
+
+                    if task.is_running() {
+                        self.current_priority = priority;
+                    }
                 },
                 State::Suspended => {
                     data.priority = priority;
@@ -346,8 +382,7 @@ impl TaskManager {
             self.suspended_tasks.push_back(task.ptr);
 
             if task.is_running() {
-                let next_task = self.forward_task();
-                self.switch_task(task, &next_task);
+                self.switch_to_next();
             }
         }
     }
@@ -362,7 +397,10 @@ impl TaskManager {
 
             self.suspended_tasks.remove(&task.ptr);
             self.push_task(task.ptr);
-            self.switch_to_next();
+
+            if data.priority > self.current_priority {
+                self.switch_to_next();
+            }
         }
     }
 
@@ -373,23 +411,21 @@ impl TaskManager {
     }
 
     fn yield_now(&mut self) {
-        self.reset_timer();
-        if self.current_tasks().len() != 1 {
+        if self.can_switch() {
             self.switch_to_next();
-        } else {
-            interrupt::enable();
         }
     }
 
+    // 内部変数を次のタスクに移行し、次のタスクを返す
     #[inline]
     fn forward_task(&mut self) -> Task {
-        // 次のタスクか最初のタスク
-        let next = Task::new(match self.running_task.data().get_next() {
-            Some(next) => next,
-            _ => {
-                self.current_priority = self.highest_priority();
-                self.current_tasks().front().unwrap()
-            }
+        let highest_priority = self.highest_priority_without(&self.running_task);
+        let next = Task::new(if self.current_priority != highest_priority {
+            self.current_priority = highest_priority;
+            self.current_tasks().front().unwrap()
+        } else {
+            // 次のタスクか最初のタスク
+            LinkedNode::get_next(self.running_task.data()).unwrap_or_else(|| self.current_tasks().front().unwrap())
         });
 
         debug_assert!(self.running_task != next);
@@ -398,17 +434,26 @@ impl TaskManager {
         self.running_task.clone()
     }
 
-    #[inline]
-    fn switch_task(&mut self, cur_task: &Task, next_task: &Task) {
+    fn switch_to_next(&mut self) {
+        self.reset_timer();
+
+        let cur_task = Task::this();
+        let next_task = self.forward_task();
         unsafe {
             arch::task::switch(&mut cur_task.data().entity, &mut next_task.data().entity);
         }
     }
 
-    pub fn switch_to_next(&mut self) {
-        let cur_task = Task::this();
-        let next_task = self.forward_task();
-        self.switch_task(&cur_task, &next_task);
+    /// スイッチした場合`true`を返す。
+    fn switch_if_needed(&mut self) -> bool {
+        let _blocker = IntBlocker::new();
+
+        if self.is_switch_needed() {
+            self.switch_to_next();
+            true
+        } else {
+            false
+        }
     }
 
     fn terminate_task(&mut self, task: &Task) {
@@ -433,6 +478,7 @@ impl TaskManager {
         if task.is_valid() {
             assert!(!task.is_running());
             self.terminate_task(task);
+            self.switch_if_needed();
         }
     }
 
