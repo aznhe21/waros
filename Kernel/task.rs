1af3fc8697543083d01612e0aafd65fd63047e3a
aznhe21 <aznhe21.mc@gmail.com>
2016-01-13 17:32:21 +0900

    forward_taskを軽量化
    次にスイッチすべき優先度を先に計算しておく

--- Kernel/task.rs
+++ Kernel/task.rs
@@ -88,12 +88,12 @@ impl Priority {
     }
 }
 
-pub struct TaskData {
-    pub id: usize,
-    pub timer: timer::Timer,
-    pub state: State,
-    pub priority: Priority,
-    pub entity: TaskEntity,
+struct TaskData {
+    id: usize,
+    timer: timer::Timer,
+    state: State,
+    priority: Priority,
+    entity: TaskEntity,
     prev: Option<Shared<TaskData>>,
     next: Option<Shared<TaskData>>
 }
@@ -237,7 +237,7 @@ struct TaskManager {
     suspended_tasks: DList<TaskData>,
     free_tasks: DList<TaskData>,
     running_task: Task,
-    current_priority: Priority,
+    next_priority: Priority,
     primary_task: Task,
     timer: timer::UnmanagedTimer,
     kcache: KCacheAllocator<TaskData>
@@ -247,7 +247,7 @@ unsafe impl Send for TaskManager { }
 unsafe impl Sync for TaskManager { }
 
 impl TaskManager {
-    #[inline(always)]
+    #[inline]
     fn init(&mut self) {
         unsafe {
             let _blocker = IntBlocker::new();
@@ -262,7 +262,7 @@ impl TaskManager {
                 suspended_tasks: DList::new(),
                 free_tasks: DList::new(),
                 running_task: primary_task.clone(),
-                current_priority: Task::DEFAULT_PRIORITY,
+                next_priority: Task::DEFAULT_PRIORITY,
                 primary_task: primary_task.clone(),
                 timer: timer::UnmanagedTimer::with_callback(TaskManager::switch_by_timer),
                 kcache: kcache
@@ -302,24 +302,14 @@ impl TaskManager {
         Priority::from_u8(priority as u8)
     }
 
-    // 指定したタスク以外で、実行可能状態タスクのある最も高い優先度を返す。
-    fn highest_priority_without(&self, task: &Task) -> Priority {
-        let (priority, _) = self.runnable_tasks.iter()
-            .enumerate()
-            .rev()
-            // !tasks.is_empty() && tasks != [task]
-            .find(|&(_, tasks)| match tasks.len() {
-                0 => false,
-                1 => tasks.front().map_or(ptr::null_mut(), |p| *p) != *task.ptr,
-                _ => true
-            })
-            .unwrap();
-        Priority::from_u8(priority as u8)
+    #[inline]
+    fn current_priority(&self) -> Priority {
+        self.running_task.data().priority
     }
 
     #[inline]
     fn current_tasks(&self) -> &DList<TaskData> {
-        &self.runnable_tasks[self.current_priority as usize]
+        &self.runnable_tasks[self.current_priority() as usize]
     }
 
     #[inline]
@@ -347,6 +337,10 @@ impl TaskManager {
             (**data).setup(task_counter.fetch_add(1, Ordering::SeqCst), entry, arg, task_terminated);
             self.push_task(data);
 
+            if (**data).priority > self.next_priority {
+                self.next_priority = (**data).priority;
+            }
+
             Task::new(data)
         }
     }
@@ -358,7 +352,7 @@ impl TaskManager {
 
     #[inline]
     fn is_switch_needed(&self) -> bool {
-        self.highest_priority() > self.current_priority
+        self.current_priority() != self.next_priority
     }
 
     #[inline]
@@ -399,12 +393,9 @@ impl TaskManager {
     }
 
     // 内部変数を次のタスクに移行し、次のタスクを返す
-    #[inline]
     fn forward_task(&mut self) -> Task {
-        let highest_priority = self.highest_priority_without(&self.running_task);
-        let next = Task::new(if self.current_priority != highest_priority {
-            self.current_priority = highest_priority;
-            self.current_tasks().front().unwrap()
+        let next = Task::new(if self.current_priority() != self.next_priority {
+            self.runnable_tasks[self.next_priority as usize].front().unwrap()
         } else {
             // 次のタスクか最初のタスク
             LinkedNode::get_next(self.running_task.data()).unwrap_or_else(|| self.current_tasks().front().unwrap())
@@ -465,10 +456,13 @@ impl TaskManager {
         debug_log!("Task terminated");
 
         interrupt::disable();
+
         let cur_task = Task::this();
-        let next_task = self.forward_task();
         let r = self.terminate_task(&cur_task);
         debug_assert!(r.is_ok());
+
+        self.next_priority = self.highest_priority();
+        let next_task = self.forward_task();
 
         self.reset_timer();
         unsafe {
@@ -491,10 +485,18 @@ impl TaskManager {
                 self.push_task(task.ptr);
 
                 if task.is_running() {
-                    self.current_priority = priority;
+                    self.next_priority = self.highest_priority();
+                } else if priority > self.next_priority {
+                    self.next_priority = priority;
                 }
             },
-            State::Suspended => data.priority = priority,
+            State::Suspended => if data.priority != priority {
+                data.priority = priority;
+
+                if priority > self.next_priority {
+                    self.next_priority = priority;
+                }
+            },
             State::Free => return Err(Error::InvalidState)
         }
 
@@ -527,6 +529,8 @@ impl TaskManager {
         self.remove_task(task.ptr);
         self.suspended_tasks.push_back(task.ptr);
 
+        self.next_priority = self.highest_priority();
+
         if task.is_running() {
             self.switch_to_next();
         }
@@ -550,7 +554,11 @@ impl TaskManager {
         self.suspended_tasks.remove(&task.ptr);
         self.push_task(task.ptr);
 
-        if now && data.priority > self.current_priority {
+        if data.priority > self.current_priority() {
+            self.next_priority = data.priority;
+        }
+
+        if now {
             self.switch_to_next();
         }
 
